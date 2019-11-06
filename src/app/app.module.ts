6b5ad74d73bd5d4bfb454708c2f18b89267ba550
Chiran_Dilanka <chirandilanka@gmail.com>
2019-11-06 10:46:54 +0530

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { AlertModule } from 'ngx-bootstrap';
+import { LoginComponent } from './login/login.component';
+import { ProjectComponent } from './project/project.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent,
+    LoginComponent,
+    ProjectComponent
   ],
+  
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+  
 })
 export class AppModule { }
